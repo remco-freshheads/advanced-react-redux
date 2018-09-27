61bdfd81908c1e285f459556f0e004ffc6f80b0c
remco-freshheads <remco.abalain@freshheads.com>
2018-09-27 11:32:52 +0200

    S2.L15

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 
 it('shows a comment box', () => {
+    const div = document.create('div');
 
+    // Render Component <App /> in div
+    ReactDOM.render(<App />, div);
+
+    // Clean up -> cleans memory when there are multiple tests
+    ReactDOM.unmountComponentAtNode(div);
 });
 
-it('shows a comment list', () => {
-
-});
+// it('shows a comment list', () => {
+//
+// });
