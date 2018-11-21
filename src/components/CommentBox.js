811994af7ec43cdef40cf1913ed9b50ea74fcb1d
remco-freshheads <remco.abalain@freshheads.com>
2018-11-21 15:53:03 +0100

    S2.L27 fixed comment change

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 export default class CommentBox extends React.Component {
+    state = {
+        comment: ''
+    };
+
+    _handleChange = event => {
+        this.setState({ comment: event.target.value });
+    };
+
+    _handleSubmit = (event) => {
+        event.preventDefault();
+
+        this.setState({ comment: '' });
+    };
+
     render() {
         return (
-            <form>
+            <form onSubmit={ this._handleSubmit }>
                 <h4>Add a comment</h4>
-                <textarea />
+                <textarea onChange={ this._handleChange } value={this.state.comment} />
                 <div>
                     <button>Submit Comment</button>
                 </div>
