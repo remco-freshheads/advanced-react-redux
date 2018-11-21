0cb08ea446cfd15303afd7c5ca19a1924197dda3
remco-freshheads <remco.abalain@freshheads.com>
2018-11-21 16:41:39 +0100

    S2.L39 create store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from 'reducers';
 
+// components
 import App from './components/App';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={createStore(reducers, {})}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 );
